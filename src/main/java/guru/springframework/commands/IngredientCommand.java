7a512c562f4323caca701a4a6dbd558f757e0e25
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2022-12-02 12:14:12 +0200

    added pojo command objects, converters from domain to command and tests

---
+++ src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -0,0 +1,16 @@
+package guru.springframework.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class IngredientCommand {
+    private Long id;
+    private String description;
+    private Double amount;
+    private UnitOfMeasureCommand uom;
+    private RecipeCommand recipe;
+}
---
+++ src/main/java/guru/springframework/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -0,0 +1,26 @@
+package guru.springframework.converters;
+
+import guru.springframework.commands.UnitOfMeasureCommand;
+import guru.springframework.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nullable;
+
+@Component
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure> {
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasure convert(@Nullable UnitOfMeasureCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(source.getId());
+        uom.setDescription(source.getDescription());
+        return uom;
+    }
+}
